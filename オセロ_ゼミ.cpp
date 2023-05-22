48aaf2499acb8c868f5427accf7f83ae866c1df1
KoTa1114 <mayotuna29@gmail.com>
2023-05-22 17:59:51 +0900

    盤面の評価関数と評価値を計算する関数を追加

--- オセロ_ゼミ.cpp
+++ オセロ_ゼミ.cpp
@@ -35,6 +35,33 @@ bool is_in(int x, int y) {
     return (x > 0 && x < 9 && y > 0 && y < 9);
 }
 
+//盤面の評価関数
+int evaluation_function[10][10] = {
+    {0,0,0,0,0,0,0,0,0,0},
+    {0,10,3,3,3,3,3,3,10,0},
+    {0,3,2,2,1,1,2,2,3,0},
+    {0,3,2,1,1,1,1,2,3,0},
+    {0,3,1,1,1,1,1,1,3,0},
+    {0,3,1,1,1,1,1,1,3,0},
+    {0,3,2,1,1,1,1,2,3,0},
+    {0,3,2,2,1,1,2,2,3,0},
+    {0,10,3,3,3,3,3,3,10,0},
+    {0,0,0,0,0,0,0,0,0,0}
+};
+
+// 盤面の評価値を計算する関数
+int Calculate_score(int player) {
+    int score = 0;
+    for(int i=0;i<10;i++) {
+        for(int j=0;j<10;j++) {
+            if(board[i][j] == player) {
+                score += evaluation_function[i][j];
+            }
+        }
+    }
+    return score;
+}
+
 //(x,y)に石を置くことができるか判定する関数
 bool Can_Put(int x,int y) {
     if(board[x][y] != 0 || x < 1 || 8 < x || y < 1 || 8 < y) return false;
